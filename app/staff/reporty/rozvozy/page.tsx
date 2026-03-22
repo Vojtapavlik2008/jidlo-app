ff2fdcd9930f7898411a955124b2ad1efef5da7b
Vojtapavlik2008 <157961053+Vojtapavlik2008@users.noreply.github.com>
2026-03-22 16:44:52 +0100

    Update page.tsx

--- app/staff/reporty/rozvozy/page.tsx
+++ app/staff/reporty/rozvozy/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import "leaflet/dist/leaflet.css";
 import Link from "next/link";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { supabase } from "@/lib/supabase";
