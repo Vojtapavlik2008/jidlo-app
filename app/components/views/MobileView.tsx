827d83fd8bd6d2354ac8bb414003892524e7a31b
Vojtapavlik2008 <157961053+Vojtapavlik2008@users.noreply.github.com>
2026-04-01 14:15:20 +0200

    Update MobileView.tsx

--- app/components/views/MobileView.tsx
+++ app/components/views/MobileView.tsx
@@ -2464,11 +2464,11 @@ export default function MobileView({ onOpenCart }: MobileViewProps) {
       </div>
     </div>
 
-    <div className="-mt-4 pl-4 text-[11px] font-semibold tracking-[0.01em] text-gray-500">
+    <div className="-mt-5 pl-6 text-[11px] font-semibold tracking-[0.01em] text-gray-500">
       rozvoz obědů po Poděbradech
     </div>
 
-    <div className="mt-2 h-[3px] w-full rounded-full bg-green-600" />
+    <div className="-mt-2 h-[3px] w-full rounded-full bg-green-600" />
   </div>
 </div>
 
